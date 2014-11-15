e35f0269320fab32faeebbaed5085d86d08f0229
Sharat M R <cosmosgenius@gmail.com>
2014-11-15 20:15:25 +0530

    moved to make agaign

--- mark2/src/main.c
+++ mark2/src/main.c
@@ -1,3 +1,16 @@
-int main() {
-    return 0;
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int
+main (void)
+{
+    DDRB |= _BV(DDB0); 
+    
+    while(1) 
+    {
+        PORTB ^= _BV(PB0);
+        _delay_ms(500);
+    }
 }
