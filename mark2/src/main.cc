9d9ae815546d4748a000da5280a38b6d102e777f
Sharat M R <cosmosgenius@gmail.com>
2014-11-20 13:25:43 +0530

    added initial code

--- mark2/src/main.cc
+++ mark2/src/main.cc
@@ -1,13 +1,48 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "wheel.h"
+
+void forward()
+{
+  PORTC &= ~(1 << 1 | 1 << 4);
+  PORTC |= (1 << 0 | 1 << 3 );
+}
+
+void backward()
+{
+  PORTC |= (1 << 1 | 1 << 4);
+  PORTC &= ~(1 << 0 | 1 << 3 );
+}
+
+void stop()
+{
+  PORTC &= ~(1 << 0 | 1 << 1 | 1 << 3 | 1 << 4);
+}
+
+void right()
+{
+
+}
+
+void left(){
+
+}
 
 int main (void)
 {
-  DDRB |= _BV(DDB0);
+  // set PORTc 0-5 as output
+  DDRC = 0x1F;
+  DDRD = 0x00;
+  PORTD = 0xFF;
+
+  PORTC = 0xff;//1 << 2 | 1 << 5;
   while(1) 
   {
-    PORTB ^= _BV(PB0);
-    _delay_ms(500);
+    if(((PIND >> 5) & 1) == 0) {
+      forward();
+    } else if ((PIND & (1 << 6)) == 0) {
+      backward();
+    } else {
+      stop();
+    }
   }
 }
